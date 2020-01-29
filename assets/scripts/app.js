71ba8f83a714e33bbcbb94f19cce7a60c8eaec49
sirajummprince <sirajummunirprince@gmail.com>
2020-01-29 23:17:16 +0600

    JS template literal in app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,8 @@
-alert('This works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3/2 -1;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
+
+outputResult(currentResult, calculationDescription);
